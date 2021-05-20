5c7e10eac270df79030c2247cc57d34f51dca163
Msgl <mariasglynou@gmail.com>
2021-05-20 10:22:53 +0300

    Basic structure, style and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Toggle navigation menu on mobile
 const toggleBtn = () => {
   const header = document.getElementById('header');
   const logo = document.getElementById('header-logo');
@@ -8,4 +9,29 @@ const toggleBtn = () => {
   header.classList.toggle('mobile-header');
   btn.classList.toggle('close-btn');
   logo.classList.toggle('mobile-logo');
+};
+
+//Feature slider
+const featureSlider = (element) => {
+  const slides = document.querySelectorAll('.slide');
+
+  slides.forEach((item) => {
+    if (element == item.id) {
+      item.classList.remove('hide');
+      item.classList.add('show');
+    } else {
+      item.classList.add('hide');
+    }
+  });
+};
+
+//FAQ Accordion
+const faqAccordion = (element) => {
+  const answers = document.querySelectorAll('.answer');
+
+  answers.forEach((item) => {
+    if (element == item.id) {
+      item.classList.toggle('hide');
+    }
+  });
 };
